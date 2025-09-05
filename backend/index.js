c8a7ae01b42cafabfe2b4c6750137040e04b79e6
Gaurav Rajput <er.gaurav2022@gmail.com>
2025-09-05 22:28:16 +0530

    refactor CORS configuration to simplify origin handling and remove unnecessary complexity

--- backend/index.js
+++ backend/index.js
@@ -8,24 +8,17 @@ const app = express();
 
 const port=process.env.PORT || 3000;
 
+
 const allowedOrigins = [
-  'http://localhost:5173',
-  'https://dsa-expert.vercel.app/'
+  "http://localhost:5173",
+  "https://dsa-expert.vercel.app"
 ];
 
 app.use(cors({
-    origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true
-}))
-
+  origin: allowedOrigins,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type"]
+}));
 app.use(express.json());
 
 app.use('/ai',aiRouter);
